b23df9f9b8a0664f59da5d5bda01fb43eaeb28d5
wli16 <wli16@demo.com>
2025-01-04 22:42:53 +0800

    Add a function to read column name. Change some parameters' name

--- SQLHandleLib.py
+++ SQLHandleLib.py
@@ -2,23 +2,29 @@ import sqlite3
 
 class SQL_Lib:
     #Connect the SQL
-    def Open_DB(self,Par_DBName):
-        return sqlite3.connect(Par_DBName)
+    def Open_DB(self, Str_DBName):
+        return sqlite3.connect(Str_DBName)
 
     #Disconnect the SQL
-    def Close_DB(self,Par_DBHandle):
-        return Par_DBHandle.close()
+    def Close_DB(self,DBHandle):
+        return DBHandle.close()
 
     #Get all table names in the SQL
-    def Get_Table_Name(self, Par_DBHandle):
-        cursor = Par_DBHandle.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+    def Get_Table_Name(self, DBHandle):
+        cursor = DBHandle.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
         Table = cursor.fetchall()
         cursor.close()
         return Table
+    #Get column name
+    def Get_Column_Name(self,DBHandle,Str_TableName):
+        cursor = DBHandle.execute(f"PRAGMA table_info({Str_TableName})")
+        column_names = [column[1] for column in cursor.fetchall()]
+        cursor.close()
+        return column_names
 
     #Get all contents in the table
-    def Get_Table_Content(self,Par_DBHandle,Par_TableName):
-        cursor = Par_DBHandle.execute(f"SELECT * FROM {Par_TableName}")
+    def Get_Table_Data(self,DBHandle,Str_TableName):
+        cursor = DBHandle.execute(f"SELECT * FROM {Str_TableName}")
         rows = cursor.fetchall()
         cursor.close()
         return rows
